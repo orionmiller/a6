b3161bd08460dc0bfc6e9c369aae8714bbf230d3
Orion Miller <orion.student@gmail.com>
2011-03-09 02:07:12 -0800

    Added detection if bad inputs for stages

--- main.c
+++ main.c
@@ -18,176 +18,188 @@ int main(void) {
    pid_t forkRes;
    int status;
    int **newPipe;
-   char **inPath = (char**) safe_malloc(sizeof(char**));
-   char **outPath = (char**) safe_malloc(sizeof(char**));
+   char *inPath;
+   char *outPath;
    FILE *in = NULL;
    FILE *out = NULL;
    int stages = 1;
    int i;
    int pipe_num = 0;
 
-      fprintf(stdout, ": ");
-      fflush(stdout);
-      *inPath = NULL;
-      *outPath = NULL;
-      /*generate stages*/
-      front = parseLine(stdin, inPath, outPath, &stages);
-      temp = front;
+   fprintf(stdout, ": ");
+   fflush(stdout);
+   inPath = NULL;
+   outPath = NULL;
+   /*generate stages*/
+   front = parseLine(stdin, &inPath, &outPath, &stages);
+   if(!(temp = front)) {
+      exit(EXIT_FAILURE);
+   }
+   /*modify for future use*/
 
-      /*DEBUG: Info*/
-      printf("inpath: %s\n", *inPath);
 
-      printf("outpath: %s\n", *outPath);
+   /*DEBUG: Info*/
+   printf("inpath: %s\n", inPath);
 
-      printf("num of pipes: %d\n", stages -1);
-      /*      i = 0;
-      while (temp) {
-         i++;
-         printStage(temp, i);
+   printf("outpath: %s\n", outPath);
+
+   printf("num of pipes: %d\n", stages -1);
+   /*      i = 0;
+           while (temp) {
+           i++;
+           printStage(temp, i);
+           temp = temp->next;
+           }
+
+           temp = front;
+   */
+   fflush(stdout);
+   /*-----------*/
+
+   /*generate pipes*/
+   if (stages > 1) {
+      newPipe  = (int**) safe_malloc(sizeof(int**) * (stages - 1));
+      for (i = 0; i < stages-1; i++) {
+         newPipe[i] = (int*) safe_malloc(sizeof(int*) * 2);
+         if (pipe(newPipe[i]) < 0) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+         }
+      }
+   }
+      
+   if (inPath) {
+      if (!(in = fopen(inPath, "r"))) {
+         perror("fopen inPath");
+         exit(EXIT_FAILURE);
+      }
+   }
+      
+   if (outPath) {
+      if (!(out = fopen(outPath, "w"))) {
+         perror("fopen outPath");
+         exit(EXIT_FAILURE);
+      }
+   }
+
+
+   if (front->next) { /*more than 1 stage*/
+      while (temp && !(forkRes=fork())) {
          temp = temp->next;
       }
-
-      temp = front;
-      */
-      fflush(stdout);
-      /*-----------*/
-
-      /*generate pipes*/
-      if (stages > 1) {
-         newPipe  = (int**) safe_malloc(sizeof(int**) * (stages - 1));
-         for (i = 0; i < stages-1; i++) {
-            newPipe[i] = (int*) safe_malloc(sizeof(int*) * 2);
-            if (pipe(newPipe[i]) < 0) {
-               perror("pipe");
-               exit(EXIT_FAILURE);
+      /*clean up pipes*/
+      fprintf(stderr, "\"%s\": forkRes: %d\n", temp->argv[0], forkRes);
+      fflush(stderr);
+      if (forkRes) { /*child*/
+         fprintf(stderr, "%s: cleaning pipes\n", temp->argv[0]);
+         fflush(stderr);
+         if (temp == front) { /*first stage*/
+            if (in) {
+               if ((dup2(fileno(in), STDIN_FILENO)) == -1)
+                  perror("1st stage dup2 in");
+            }
+            if ((dup2(newPipe[pipe_num][WRITE_END], STDOUT_FILENO)) == -1)
+               perror("1st stage dup2 out");
+            if((close(newPipe[pipe_num][READ_END])) == -1)
+               perror("1st stage close READ_END");
+            /*close other pipes*/
+            for (i = 0; i < stages - 1; i++) {
+               if (i != pipe_num) {
+                  if((close(newPipe[i][READ_END])) == -1)
+                     perror("stage close READ_END");
+                  if((close(newPipe[i][WRITE_END])) == -1)
+                     perror("stage close WRITE_END");
+               }
             }
          }
-      }
-      
-      if (*inPath) {
-         if (!(in = fopen(*inPath, "r"))) {
-            perror("fopen inPath");
-            exit(EXIT_FAILURE);
+         else if (temp->next == NULL) { /*final stage*/
+            if (out) {
+               if ((dup2(fileno(out), STDOUT_FILENO)) == -1)
+                  perror("last stage dup2 in");
+            }
+            if ((dup2(newPipe[pipe_num][READ_END], STDIN_FILENO)) == -1)
+               perror("last stage dup2 out");
+            if((close(newPipe[pipe_num][WRITE_END])) == -1)
+               perror("last stage close RITE_END");
+            /*close other pipes*/
+            for (i = 0; i < stages - 1; i++) {
+               if (i != pipe_num) {
+                  if((close(newPipe[i][READ_END])) == -1)
+                     perror("stage close READ_END");
+                  if((close(newPipe[i][WRITE_END])) == -1)
+                     perror("stage close WRITE_END");
+               }
+            }
          }
-      }
-      
-      if (*outPath) {
-         if (!(out = fopen(*outPath, "w"))) {
-            perror("fopen outPath");
-            exit(EXIT_FAILURE);
-         }
-      }
-
-      while (temp) {
-         if (!(forkRes=fork())) {
-            /*clean up pipes*/
-
-            if (front->next) { /*more than 1 stage*/
-               if (temp == front) { /*first stage*/
-                  if (in) {
-                     if ((dup2(fileno(in), STDIN_FILENO)) == -1)
-                        perror("1st stage dup2 in");
-                  }
-                  if ((dup2(newPipe[pipe_num][WRITE_END], STDOUT_FILENO)) == -1)
-                     perror("1st stage dup2 out");
-                  if((close(newPipe[pipe_num][READ_END])) == -1)
-                     perror("1st stage close READ_END");
-                  /*close other pipes*/
-                  for (i = 0; i < stages - 1; i++) {
-                     if (i != pipe_num) {
-                        if((close(newPipe[i][READ_END])) == -1)
-                           perror("stage close READ_END");
-                        if((close(newPipe[i][WRITE_END])) == -1)
-                           perror("stage close WRITE_END");
-                     }
-                  }
-               }
-               else if (temp->next == NULL) { /*final stage*/
-                  if (out) {
-                     if ((dup2(fileno(out), STDOUT_FILENO)) == -1)
-                        perror("last stage dup2 in");
-                  }
-                  if ((dup2(newPipe[pipe_num][READ_END], STDIN_FILENO)) == -1)
-                     perror("last stage dup2 out");
-                  if((close(newPipe[pipe_num][WRITE_END])) == -1)
-                     perror("last stage close RITE_END");
-                  /*close other pipes*/
-                  for (i = 0; i < stages - 1; i++) {
-                     if (i != pipe_num) {
-                        if((close(newPipe[i][READ_END])) == -1)
-                           perror("stage close READ_END");
-                        if((close(newPipe[i][WRITE_END])) == -1)
-                           perror("stage close WRITE_END");
-                     }
-                  }
-               }
-               else { /*middle stages*/
-                  if ((dup2(newPipe[pipe_num][READ_END], STDIN_FILENO)) == -1)
-                     perror("stage dup2 READ_END");
-                  if ((dup2(newPipe[pipe_num+1][WRITE_END], STDOUT_FILENO)) == -1)
-                     perror("stage dup2 WRITE_END");
-                  
-                  
-                  if((close(newPipe[pipe_num][WRITE_END])) == -1)
-                     perror("stage close WRITE_END 1");
-                  if((close(newPipe[pipe_num+1][READ_END])) == -1)
-                     perror("stage close READ_END 2");
-                  
-                  for (i = 0; i < stages - 1; i++) {
-                     if (i != pipe_num && i != pipe_num + 1) {
-                        printf("i: %d pipe_num: %d \n", i, pipe_num);
-                        fflush(stdout);
-                        if((close(newPipe[i][READ_END])) == -1)
-                           perror("stage close READ_END 3");
-                        if((close(newPipe[i][WRITE_END])) == -1)
-                           perror("stage close WRITE_END 4");
-                     }
-                  }
-                  pipe_num++;
-               }
+         else { /*middle stages*/
+            if ((dup2(newPipe[pipe_num][READ_END], STDIN_FILENO)) == -1)
+               perror("stage dup2 READ_END");
+            if ((dup2(newPipe[pipe_num+1][WRITE_END], STDOUT_FILENO)) == -1)
+               perror("stage dup2 WRITE_END");
                
-            }
-            else { /*only one stage*/
-               if (in) {
-                  if ((dup2(fileno(in), STDIN_FILENO)) == -1)
-                     perror("only 1 stage dup2 in");
+               
+            if((close(newPipe[pipe_num][WRITE_END])) == -1)
+               perror("stage close WRITE_END 1");
+            if((close(newPipe[pipe_num+1][READ_END])) == -1)
+               perror("stage close READ_END 2");
+               
+            for (i = 0; i < stages - 1; i++) {
+               if (i != pipe_num && i != pipe_num + 1) {
+                  printf("i: %d pipe_num: %d \n", i, pipe_num);
+                  fflush(stdout);
+                  if((close(newPipe[i][READ_END])) == -1)
+                     perror("stage close READ_END 3");
+                  if((close(newPipe[i][WRITE_END])) == -1)
+                     perror("stage close WRITE_END 4");
                }
-               if (out) {
-                  if ((dup2(fileno(out), STDOUT_FILENO)) == -1)
-                     perror("only 1 stage dup2 out");
-               }
             }
-            
-            /*execute*/
-            fprintf(stderr, "command: \"%s\"\n", temp->argv[0]);
-            fflush(stderr);
-            execv(temp->argv[0] ,temp->argv);
-            perror(temp->argv[0]);
-            exit(EXIT_FAILURE);
-            /*TODO: check for res error*/
-         }
-         else {
-            temp = temp->next;
+            pipe_num++;
          }
       }
-      wait(&status);
-      if (WIFEXITED(status) && WEXITSTATUS(status)) {
-         perror("program child exited with an error");
-      }
+   }
+   else { /*only one stage*/
       if (in) {
-         if((fclose(in)) != 0) {
-            perror("fclose in");
-         }
-      } 
-      if (out) {
-         if((fclose(out)) != 0) {
-            perror("fclose in");
-         }
+         if ((dup2(fileno(in), STDIN_FILENO)) == -1)
+            perror("only 1 stage dup2 in");
       }
-      in = NULL;
-      out = NULL;
+      if (out) {
+         if ((dup2(fileno(out), STDOUT_FILENO)) == -1)
+            perror("only 1 stage dup2 out");
+      }
+   }
 
-      fflush(stdin);
+   /*execute*/
+   if (temp) {
+      execv(temp->argv[0] ,temp->argv);
+      perror(temp->argv[0]);
+      exit(EXIT_FAILURE);
+   }
 
+   /*TODO: check for res error*/
+
+
+   wait(&status);
+   if (WIFEXITED(status) && WEXITSTATUS(status)) {
+      perror("program child exited with an error");
+   }
+   fprintf(stderr, "parent finished wait\n");
+   fflush(stderr);
+   fprintf(stdout, "parent finished wait\n");
+   fflush(stdout);
+   if (in) {
+      if((fclose(in)) != 0) {
+         perror("fclose in");
+      }
+   } 
+   if (out) {
+      if((fclose(out)) != 0) {
+         perror("fclose in");
+      }
+   }
+   in = NULL;
+   out = NULL;
+      
+   fflush(stdin);
+      
    return EXIT_SUCCESS;
 }
